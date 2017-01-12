80571c7be08896d2875f488d1a6f8c6c19b54d95
GEORGE QUENTIN <ggeoo93@gmail.com>
2017-01-12 22:31:56 +0000

    initial

---
+++ StorySmartiesGames/StorySmartiesGames/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  StorySmartiesGames
+//
+//  Created by GEORGE QUENTIN on 12/01/2017.
+//  Copyright © 2017 GEORGE QUENTIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var cardView: UIView!
+    var back: UIImageView!
+    var front: UIImageView!
+    
+    var showingBack = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.red
+        front = UIImageView(image: UIImage(named: "front.png"))
+        back = UIImageView(image: UIImage(named: "back.png"))
+        
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(ViewController.tapped))
+        singleTap.numberOfTapsRequired = 1
+        
+        let rect = CGRect(x: 20, y: 20, width: (back.image?.size.width)!, height: (back.image?.size.height)!)
+        cardView = UIView(frame: rect)
+        cardView.addGestureRecognizer(singleTap)
+        cardView.isUserInteractionEnabled = true
+        cardView.addSubview(back)
+        
+        view.addSubview(cardView)
+    }
+    
+    func tapped() {
+        if (showingBack) {
+        UIView.transition(from: back, to: front, duration: 1, options: UIViewAnimationOptions.transitionFlipFromRight, completion: nil)
+        showingBack = false
+        } else {
+        UIView.transition(from: front, to: back, duration: 1, options: UIViewAnimationOptions.transitionFlipFromLeft, completion: nil)
+        showingBack = true
+        }
+        
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
